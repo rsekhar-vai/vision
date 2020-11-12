b61f803a8c63cca5092f0a27c149bebd7cc7b10b
Raja-V <raja.valiveti@gmail.com>
2020-11-12 14:43:33 +0530

    Added Batchnorm to model

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torchvision
 import numpy as np
 from utils.functions import fit_model
-from models import SimpleConv
+from models import *
 
 data_path = "data/"
 data_train = torchvision.datasets.CIFAR10(data_path,train=True,download=True,
@@ -11,6 +11,10 @@ data_train = torchvision.datasets.CIFAR10(data_path,train=True,download=True,
 data_test = torchvision.datasets.CIFAR10(data_path,train=False,download=True,
                                          transform=torchvision.transforms.ToTensor())
 
+
+
+model = ConvWithBnorm()
+results = fit_model(data_train,data_test,model)
 
 model =  SimpleConv()
 results = fit_model(data_train,data_test,model)
